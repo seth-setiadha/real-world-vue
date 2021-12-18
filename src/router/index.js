ada93ad12094fbdd419fa7ddf768f506f2ec5b22
Seth Setiadha <seth.setiadha@gmail.com>
2021-12-17 23:45:48 -0500

    First commit Real World Vue 3

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router"
+import EventList from "@/views/EventList.vue"
+import EventDetails from "@/views/EventDetails.vue"
+import About from "@/views/About.vue"
+
+
+const routes = [
+  {
+    path: "/",
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails,    
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;

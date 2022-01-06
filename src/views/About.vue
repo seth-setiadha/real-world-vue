1e38f07986962883b2887f798fcef98f072aad9d
Seth Setiadha <seth.setiadha@gmail.com>
2022-01-06 10:24:05 -0500

    typo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>Oct 2021,Tutorial projects Real World Vue from VusMastery</h1>
+    <h1>Dec 2021,Tutorial projects Real World Vue from VueMastery</h1>
   </div>
 </template>
